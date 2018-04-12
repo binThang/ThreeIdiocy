d1787d3151bc309e40178752fb456fe8408a9411
YongSubKwak <iddqt@naver.com>
2018-04-12 10:33:14 +0900

    백준 이동하기

---
+++ 백준_이동하기/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int ary[1000][1000] = { 0 };
+int maximum[1000][1000] = { 0 };
+
+int main()
+{
+
+	// read input
+	int n, m;
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> ary[i][j];
+		}
+	}
+
+	//
+	maximum[0][0] = ary[0][0];
+
+	// traverse ary
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			int upperValue = (0 <= (i - 1) && (i - 1) < n) ? maximum[i - 1][j] : 0;
+			int leftValue = (0 <= (j - 1) && (j - 1) < m) ? maximum[i][j - 1] : 0;
+			int leftupperValue = ((0 <= (i - 1) && (i - 1) < n) && (0 <= (j - 1) && (j - 1) < m)) ? maximum[i - 1][j - 1] : 0;
+
+			maximum[i][j] = _Max_value(upperValue, _Max_value(leftValue, leftupperValue)) + ary[i][j];
+		}
+	}
+
+	// print
+	cout << maximum[n-1][m-1];
+
+	return 0;
+}
